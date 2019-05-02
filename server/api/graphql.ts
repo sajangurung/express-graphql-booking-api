c79a976e9158d71f7f87ecb52c18cb6d0c0c9e1c
Sajan Gurung <sjnkon@gmail.com>
2019-05-02 21:48:53 +1000

    add mutation for creatEvent

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -1,14 +1,37 @@
 import * as graphql from 'express-graphql';
 import { buildSchema } from 'graphql';
+import Event from '../../server/models/Event';
 
 const graphqlApi = graphql({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      userId: ID!
+      title: String!
+      tagline: String!
+      description: String!
+      label: String!
+      length: String!
+      maxPeople: Int!
+      createdAt: String!
+    }
+
+    input EventInput {
+      userId: String!
+      title: String!
+      tagline: String!
+      description: String!
+      label: String!
+      length: String!
+      maxPeople: Int!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): String
     }
 
     schema {
@@ -21,7 +44,15 @@ const graphqlApi = graphql({
       return ['events', 'test'];
     },
     createEvent: (args: any) => {
-      return args.name;
+      const eventInput = args.eventInput;
+
+      return Event.createEvent(eventInput)
+        .then(response => {
+          return response._id;
+        }).catch(error => {
+          throw error;
+        });
+
     },
   },
   graphiql: true,
